2d771681a9504dbeddf15b63ca3977809e8b170d
Ciro <Ciro16@gmail.com>
2020-06-18 14:32:19 -0600

    integrated hashmap with linkedlist

--- lib/p05_hash_map.rb
+++ lib/p05_hash_map.rb
@@ -1,6 +1,8 @@
 require_relative 'p04_linked_list'
 
 class HashMap
+
+  include Enumerable
   attr_accessor :count
 
   def initialize(num_buckets = 8)
@@ -9,27 +11,42 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    if num_buckets == @count
+      resize!
+    end
+    if include?(key)
+      bucket(key).update(key,val)
+    else
+      bucket(key).append(key,val)
+      @count += 1
+    end
   end
 
   def get(key)
+    bucket(key).get(key)      
   end
 
   def delete(key)
+    bucket(key).remove(key)
+    @count -= 1
   end
 
   def each
+    @store.each do |linked_list|
+      linked_list.each {|list| yield [list.key, list.val]}
+    end
   end
 
-  # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -41,9 +58,18 @@ class HashMap
   end
 
   def resize!
+    old_store = @store.dup
+    @store = Array.new(num_buckets*2) {LinkedList.new}
+    @count = 0
+    old_store.each do |bucket|
+      bucket.each do |link|
+        set(link.key,link.val)
+      end
+    end
   end
 
   def bucket(key)
+    @store[key.hash % num_buckets]
     # optional but useful; return the bucket corresponding to `key`
   end
 end
