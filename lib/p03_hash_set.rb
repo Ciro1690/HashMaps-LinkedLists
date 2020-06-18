357bbe1b26198d1ef71eb4caeabb8a29418fc17b
Ciro <Ciro16@gmail.com>
2020-06-17 18:42:36 -0600

    created hash set

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -7,12 +7,31 @@ class HashSet
   end
 
   def insert(key)
+    if @store.length == @count
+      resize!
+    end
+    
+    stored_value = key.hash % num_buckets
+    @store.each_with_index do |array,i|
+      if !array.include?(key) &&  i == stored_value
+        array << key
+        @count += 1
+      end
+    end
   end
 
   def include?(key)
+    @store.any?{|array| array.include?(key)}
+
   end
 
   def remove(key)
+    @store.each do |array|
+      if array.include?(key)
+        array.delete(key)
+        @count-=1
+      end
+    end
   end
 
   private
@@ -26,5 +45,11 @@ class HashSet
   end
 
   def resize!
+    old_store = @store.dup
+    @store = Array.new(num_buckets*2) {Array.new}
+    @count = 0
+    old_store.flatten.each do |ele|
+      insert(ele)
+    end
   end
 end
